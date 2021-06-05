1f04dce2dc2bf6b8028b1e32c273680a0f9cf559
Robyn Taylor <rhtaylor0715@gmail.com>
2021-06-05 20:56:16 +0000

    Automatically backed up by Learn

---
+++ lib/count_sentences.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class String
+
+  def sentence?
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
+  end
+
+  def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
+  end
+
+  def exclamation?
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
+  end
+  
+  def count_sentences
+    array = self.split(/[\.!?] /)
+    array.length
+  end
+end
